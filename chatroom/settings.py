418a3bdf31d8ccdba5718339c089e4fbba02ef98
salmanmaqsoodlive <isalmanmaqsood@gmail.com>
2024-01-05 12:28:11 +0500

    allowed orgin

--- chatroom/settings.py
+++ chatroom/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
 ]
 
-CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000"]
+CORS_ALLOWED_ORIGINS = ["http://localhost:3000", "http://127.0.0.1:3000", "https://react-chat-app-mu-eight.vercel.app"]
 
 ROOT_URLCONF = "chatroom.urls"
 
